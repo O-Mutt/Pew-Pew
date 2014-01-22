91712c463efb80d38ffc8645f422b3892632d88a
Matt Erickson <Matt@MattErickson.ME>
2014-01-21 21:43:30 -0600

    More updates

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+    	console.log("bind device ready");
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
